4f4e5e7e4c4ea2e31099c3e1714b5534f11b9101
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-06 13:12:48 -0500

    comments

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+//Button 1 argues
 function argue() {
     let p = document.getElementById("argue_text");
     p.innerText = "I'm Right";
 }
-
+//Button 2 argues back
 function argueMore() {
     let p = document.getElementById("argue_text");
     p.innerText = "No, I'm Right";
 }
 
+//Function telling not to hover
 function dontHover() {
     alert("I told you not to hover over me!");
 }
 
+//Event listener for the form, which checks the password credentials
 document.getElementById("submit").addEventListener("click", function(event){
     event.preventDefault();
     let pass = document.getElementById("password").value;
